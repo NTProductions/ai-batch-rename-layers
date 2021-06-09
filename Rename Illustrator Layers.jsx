a696d84c18cb7149538c10520eba2969b13a4d81
Nate Lovell <Natelovell99@gmail.com>
2021-06-09 03:33:27 -0600

    Create Rename Illustrator Layers.jsx

---
+++ Rename Illustrator Layers.jsx
@@ -0,0 +1,18 @@
+﻿main();
+function main() {
+
+if(app.activeDocument != null) {
+var document = app.activeDocument;
+var layers = document.layers;
+
+var layer;
+for(var l = 0; l < layers.length; l++) {
+    layer = layers[l];
+    layer.name = "New Layer Name "+ l;
+    }
+} else {
+            alert("No Document selected");
+            return false;
+            }
+
+}
